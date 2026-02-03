b8d76ddc445a376bc1f49b623255265a807a3d81
Ivan <74571420+ivancodes1@users.noreply.github.com>
2026-02-03 19:05:22 +0100

    Altri piccoli miglioramenti + Test pronto per la presentazione

--- scripts/Test.m
+++ scripts/Test.m
@@ -1,7 +1,8 @@
 clear;
 
-[ A, t, U ] = buildsyntheticdata(n_nodi=40,n_esperimenti=20,n_t=50);
+[ A, t, U ] = buildsyntheticdata(n_nodi=40,n_esperimenti=40,n_t=50);
 
+% CAMBIARE IL NUMERO DI ESPERIMENTI SOPRA!
 A_rec = solutore_1(U, t);
 %A_rec = solutore_2(U, t);
 %A_rec = solutore_sliding(U, t, window=2, step=1);
--- scripts/statistiche.m
+++ scripts/statistiche.m
@@ -14,19 +14,20 @@ function statistiche(A, A_rec)
     tot_archi_reali = sum(tri_A(:));
     tot_archi_rec = sum(tri_Rec(:));
     
-    accuratezza = (TP + TN) / numel(tri_A) * 100;
-    precisione = TP / (TP + FP) * 100; % Quanti dei trovati sono corretti?
-    recupero = TP / (TP + FN) * 100;    % Quanti dei reali abbiamo trovato?
+    % Accuratezza = Quante caselle della matrice ho indovinato in totale
+    accuratezza = (TP+TN)/numel(tri_A)*100; % "numel" estituisce il numero di elementi n nell'array A
+    precisione = TP/(TP+FP)*100; % Delle connessioni che sono state ricostruite, quante sono vere
+    recupero = TP/(TP+FN)*100; % Quante connessioni vere ho trovato
     
-    %% Stampa Risultati
-    fprintf('--- Risultati Ricostruzione ---\n');
-    fprintf('Archi Reali: %d\n', tot_archi_reali);
-    fprintf('Archi Ricostruiti: %d\n', tot_archi_rec);
+    %% Stampa risultati
+    fprintf('--- Risultati ricostruzione ---\n');
+    fprintf('Archi reali: %d\n', tot_archi_reali);
+    fprintf('Archi ricostruiti: %d\n', tot_archi_rec);
     fprintf('-------------------------------\n');
-    fprintf('Falsi Positivi (Archi Fantasma): %d\n', FP);
-    fprintf('Falsi Negativi (Archi Persi):    %d\n', FN);
+    fprintf('Falsi positivi: %d\n', FP);
+    fprintf('Falsi negativi: %d\n', FN);
     fprintf('-------------------------------\n');
-    fprintf('Accuratezza Globale:  %.2f%%\n', accuratezza);
-    fprintf('Precisione:           %.2f%%\n', precisione);
-    fprintf('Archi veri trovati:   %.2f%%\n', recupero);
+    fprintf('Accuratezza globale: %.2f%%\n', accuratezza);
+    fprintf('Precisione: %.2f%%\n', precisione);
+    fprintf('Archi veri trovati: %.2f%%\n', recupero);
 end
